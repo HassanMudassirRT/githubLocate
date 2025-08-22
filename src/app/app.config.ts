e357b1ceba19e82e635d8f6db42250256a5597dc
HassanMudassirRT <hassan.mudassir@rolustech.com>
2025-08-22 15:39:04 +0500

    feat: setup theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import Noir from './lib/noir';
 
 import { routes } from './app.routes';
 
@@ -18,7 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: Noir,
       },
     }),
   ],
