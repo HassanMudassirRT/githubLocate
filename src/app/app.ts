4d6d5e996c4796ac7e64cde4afd849dd39f7e7b2
HassanMudassirRT <hassan.mudassir@rolustech.com>
2025-08-22 16:27:50 +0500

    chore: setup feature-based folder structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('githubLocate');
+export class App implements OnInit {
   constructor(private primeng: PrimeNG) {}
 
   ngOnInit() {
