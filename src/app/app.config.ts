52dc73f5cb4b8947452e212915a7be23c1d7fe2a
HassanMudassirRT <hassan.mudassir@rolustech.com>
2025-08-28 17:30:44 +0500

    feat: add fade transitions on route change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding} from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions} from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -11,7 +11,7 @@ import { errorInterceptor } from './lib/interceptors/error';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({
